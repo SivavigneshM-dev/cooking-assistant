a981f556735e2b4cb1aafc6abfc3f5b456b25a7e
Siva010q <suryaarts110@gmail.com>
2025-11-14 21:41:16 +0530

    deleted

--- dishes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name='main'),
-    path('dishes/', views.dish_list, name='dish_list'),
-    path('dishes/details/<int:id>/', views.dish_detail, name='dish_detail'),
-]
-
