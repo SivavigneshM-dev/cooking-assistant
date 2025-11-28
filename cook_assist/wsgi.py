d9a65c23a4dcf1aa3c6ed1043bfc75a2ff682ca5
Siva010q <suryaarts110@gmail.com>
2025-11-28 15:04:45 +0530

    urls.py

--- cook_assist/wsgi.py
+++ cook_assist/wsgi.py
@@ -12,7 +12,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cooking_assistant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cook_assist.settings')
 
 application = get_wsgi_application()
 app = application  # <--- THIS IS REQUIRED FOR VERCEL
